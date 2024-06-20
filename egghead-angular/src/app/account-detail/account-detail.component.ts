efed2a04e055b815ec74e423268e5c572d132f75
Damian Hall <damian@damianhall.net>
2024-06-20 11:52:17 -0400

    Video 15 params fix

--- egghead-angular/src/app/account-detail/account-detail.component.ts
+++ egghead-angular/src/app/account-detail/account-detail.component.ts
@@ -1,22 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, ParamMap, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-account-detail',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   template: `
     <p>
       Account #{{this.id}} works!
     </p>
+    <ul>
+    <li><a [routerLink]="['/account', 1]">One</a></li>
+    <li><a [routerLink]="['/account', 2]">Two</a></li>
+    <li><a [routerLink]="['/account', 3]">Three</a></li>
+</ul>
   `,
   styles: ``
 })
 export class AccountDetailComponent implements OnInit{
   id!: string;
+
   constructor(private route:ActivatedRoute) {}
+  
   ngOnInit(): void {
-    this.id = this.route.snapshot.paramMap.get('id') ?? '';
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      this.id = params.get('id') ?? '';
+    });
   }
 
 }
